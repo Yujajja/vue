0215f4f38d487a81edf0fddaa8231ae5bae96682
Yujajja <150987129+Yujajja@users.noreply.github.com>
2025-03-04 15:22:49 +0900

    Add files via upload

---
+++ photosData.js
@@ -0,0 +1,49 @@
+export default [
+    {
+      id: 0,
+      name: "부서지는 파도의 한 조각",
+      date: 20220124,
+      note:
+        "바다에서의 추억을 노트를 기록해 보세요!",
+      img:
+        "https://images.unsplash.com/photo-1532467411038-57680e3dc0f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      id: 1,
+      name: "나른한 오후의 기록",
+      date: 20230420,
+      note:
+        "책방에서의 추억을 노트에 기록해 보세요!",
+      img:
+        "https://images.unsplash.com/photo-1589271243979-3153ef0dcbd7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      id: 2,
+      name: "산들바람과 함께하는 산책",
+      date: 20220806,
+      note:
+        "비 오는 날의 추억을 노트를 기록해 보세요!",
+      img:
+        "https://images.unsplash.com/photo-1626437513709-7ed95e5d0008?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      id: 3,
+      name: "무화과가 가장 맛있을 계절",
+      date: 20220201,
+      note:
+        "가을의 추억을 노트를 기록해 보세요!",
+      img:
+        "https://images.unsplash.com/photo-1586871309494-e05cd95c3cfc?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      id: 4,
+      name: "꽃은 지고 하늘은 높고,",
+      date: 20220821,
+      note: "행복한 날의 추억을 노트를 기록해 보세요!",
+      img:
+        "https://images.unsplash.com/photo-1628430382631-4fe0be228ddd?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  ];
+
+  
+  
